4c6cfe2f37f21a54fc4fa341ba4fcd839ea17fab
Danil <91548049+Danil956ya@users.noreply.github.com>
2022-06-21 13:08:03 +0300

    Create 5.3.cs

---
+++ 5.3.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace _5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            string message = "";
+
+            while (message != "exit")
+            {
+                Console.WriteLine("Введите число, exit или sum: ");
+                message = Console.ReadLine();
+                int TempInt;
+
+                if (int.TryParse(message,out TempInt) == true)
+                {
+                    AddNumber(numbers, message);
+                }
+                else if (message == "sum")
+                {
+                    GetSum(numbers);
+                }
+                else
+                {
+                    Console.WriteLine("Нет такой команды.");
+                }
+            }
+        }
+        static void AddNumber(List<int> dada, string message)
+        {
+            dada.Add(Convert.ToInt32(message));
+        }
+
+        static void GetSum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine("Сумма массива: {0}", sum);
+        }
+    }
+}
