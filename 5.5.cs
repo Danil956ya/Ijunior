41ca918d73e4fc3602c9124550ec63e13e1ea245
Danil <91548049+Danil956ya@users.noreply.github.com>
2022-06-22 13:43:59 +0300

    Create 5.5.cs

---
+++ 5.5.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace _5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = { 1, 2, 1};
+            int[] secondArray = { 3, 2 };
+            List<int> list = new List<int>();
+
+            AddToList(list, firstArray.Length, firstArray);
+            AddToList(list, secondArray.Length, secondArray);
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void AddToList(List<int> list, int arrayLength, int[] array)
+        {
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (list.Contains(array[i]) == false)
+                {
+                    list.Add(array[i]);
+                }
+            }
+        }
+    }
+}
