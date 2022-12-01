d2cfb3fc085934a8a33d448d0695052ef2143e01
Danil <91548049+Danil956ya@users.noreply.github.com>
2022-12-01 15:09:35 +0300

    Update 6.9.cs

--- 6.9.cs
+++ 6.9.cs
@@ -11,7 +11,7 @@ namespace _1._4
 
     class Market
     {
-        private List<Good> _products = new List<Good>();
+        private List<Product> _products = new List<Product>();
         private Queue<Client> _clients = new Queue<Client>();
         private int _minClients = 3;
         private int _maxClients = 10;
@@ -21,35 +21,28 @@ namespace _1._4
             Random random = new Random();
             int countClients = random.Next(_minClients, _maxClients);
 
-            _products.Add(new Good("Молоко", 50));
-            _products.Add(new Good("Морковь", 20));
-            _products.Add(new Good("Макароны", 35));
-            _products.Add(new Good("Картошка", 20));
-            _products.Add(new Good("Лук", 20));
-            _products.Add(new Good("Печенье", 35));
-            _products.Add(new Good("Шоколад", 60));
-            _products.Add(new Good("Хлеб", 35));
-            _products.Add(new Good("Доширак", 25));
-            _products.Add(new Good("Гречка", 60));
-            _products.Add(new Good("Рис", 60));
-            _products.Add(new Good("Кола", 45));
-            _products.Add(new Good("Фанта", 45));
+            _products.Add(new Product("Молоко", 50));
+            _products.Add(new Product("Морковь", 20));
+            _products.Add(new Product("Макароны", 35));
+            _products.Add(new Product("Картошка", 20));
+            _products.Add(new Product("Лук", 20));
+            _products.Add(new Product("Печенье", 35));
+            _products.Add(new Product("Шоколад", 60));
+            _products.Add(new Product("Хлеб", 35));
+            _products.Add(new Product("Доширак", 25));
+            _products.Add(new Product("Гречка", 60));
+            _products.Add(new Product("Рис", 60));
+            _products.Add(new Product("Кола", 45));
+            _products.Add(new Product("Фанта", 45));
 
             for (int i = 0; i < countClients; i++)
             {
-                _clients.Enqueue(new Client());
+                _clients.Enqueue(new Client(_products));
             }
         }
 
         public void ServeClients()
         {
-
-            foreach (var client in _clients)
-            {
-
-                client.AddGoods(_products);
-            }
-
             while (_clients.Count > 0)
             {
                 Console.Clear();
@@ -66,9 +59,11 @@ namespace _1._4
                     _clients.Dequeue();
                     Console.WriteLine("\nКлиент оплатил покупки.");
                 }
+
                 Console.WriteLine("\nНажмите любую кнопку.");
                 Console.ReadKey();
             }
+
             Console.WriteLine("Всех обслужили.");
         }
 
@@ -76,18 +71,37 @@ namespace _1._4
 
     class Client
     {
-        private List<Good> _products = new List<Good>();
-        private int _minMoney = 100;
-        private int _maxMoney = 500;
+        private List<Product> _products = new List<Product>();
         private int _minGoods = 5;
         private int _maxGoods = 12;
 
         public int Money { get; private set; }
+        public int Bagage { get; private set; }
 
-        public Client()
+        public Client(List<Product> products)
         {
             Random random = new Random();
-            Money = random.Next(_minMoney, _maxMoney);
+            int minMoney = 100;
+            int maxMoney = 500;
+            Money = random.Next(minMoney, maxMoney);
+            Bagage = random.Next(_minGoods, _maxGoods);
+
+            for (int i = 0; i < Bagage; i++)
+            {
+                _products.Add(GetRandomGood(products));
+            }
+        }
+
+        public Product GetRandomGood(List<Product> products)
+        {
+            Random random = new Random();
+            int randomGood = random.Next(0, products.Count);
+            return products[randomGood];
+        }
+
+        public bool IsMoneyEnough()
+        {
+            return Money >= GetTotalPrice();
         }
 
         public void RemoveRandomProduct()
@@ -98,31 +112,14 @@ namespace _1._4
             _products.RemoveAt(randomGood);
         }
 
-        public bool IsMoneyEnough()
-        {
-            return Money >= GetTotalPrice();
-        }
-
         public void ShowStat()
         {
             ShowGoods();
             Console.WriteLine($"\nСтоймость покупок - {GetTotalPrice()}    Денег у клиента - {Money}.");
         }
 
-        public void AddGoods(List<Good> goods)
-        {
-            Random random = new Random();
-            int randomGoods = random.Next(_minGoods,_maxGoods);
-
-            for (int i = 0; i < randomGoods; i++)
-            {
-                _products.Add(goods[i].GetRandomGood(goods));
-            }
-        }
-
         private void ShowGoods()
         {
-
             foreach (var product in _products)
             {
                 product.ShowStat();
@@ -133,33 +130,24 @@ namespace _1._4
         {
             int total = 0;
 
-            foreach(var good in _products)
+            foreach (var good in _products)
             {
                 total += good.Price;
             }
+
             return total;
         }
     }
 
-    class Good
+    class Product
     {
-
         public string Name { get; private set; }
         public int Price { get; private set; }
 
-        public Good() { }
-
-        public Good(string name, int price)
+        public Product(string name, int price)
         {
             Name = name;
             Price = price;
-        }
-
-        public Good GetRandomGood(List<Good> goods)
-        {
-            Random random = new Random();
-            int randomGood = random.Next(0, goods.Count);
-            return goods[randomGood];
         }
 
         public void ShowStat()
@@ -167,5 +155,4 @@ namespace _1._4
             Console.WriteLine($"{Name} - {Price}");
         }
     }
-
 }
